f979543d74cc512dcd4ebbd3fca44e703980ed1a
brage-andreas <bragerino+github@gmail.com>
2026-02-03 10:13:01 +0100

    feat: connect `User` and `NoteData`

--- backend/src/Models/NoteData.cs
+++ backend/src/Models/NoteData.cs
@@ -4,14 +4,17 @@ namespace Backend.Models;
 
 public class NoteData
 {
-    public Guid Id { get; set; }
-    public string Note { get; set; }
-    public DateTime Time { get; set; }
+    public required Guid Id { get; set; }
+    public required string Note { get; set; }
+    public required DateTime Time { get; set; }
+    public required string UserId { get; set; }
+    public required User User { get; set; }
 
-    public NoteData(Guid id, string note, DateTime time)
+    public NoteData(Guid id, string note, DateTime time, string userId)
     {
         Id = id;
         Note = note;
         Time = time;
+        UserId = userId;
     }
 }
--- backend/src/Models/User.cs
+++ backend/src/Models/User.cs
@@ -6,5 +6,17 @@ public class User
     public required string Username { get; set; }
     public required string Email { get; set; }
     public required string PasswordHash { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public required DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public required UserRole Role { get; set; }
+    public required ICollection<NoteData> Notes { get; set; } = [];
+    public string? LocationId { get; set; } 
+    /// <summary>
+    /// The location associated with the user. If the user type is Worker, the user should not set a value and instead
+    /// inherit the location from their supervisor.
+    /// </summary>
+    public required Location Location { get; set; }
+    public string? ManagerId { get; set; }
+    public User? Manager { get; set; }
+    public ICollection<User> Managers { get; set; } = [];
+    public ICollection<User> Subordinates { get; set; } = [];
 }
