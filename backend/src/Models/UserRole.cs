3398cd82dd098c2a041f4169ae3cd4b73427e661
brage-andreas <bragerino+github@gmail.com>
2026-02-03 10:12:04 +0100

    feat: add `UserRole`

---
+++ backend/src/Models/UserRole.cs
@@ -0,0 +1,11 @@
+public enum UserRole
+{
+    /// <summary>
+    /// An operator is a regular worker/employee.
+    /// </summary>
+    Operator,
+    /// <summary>
+    /// A foreman is a supervisor.
+    /// </summary>
+    Foreman,
+}
