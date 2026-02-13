592568a2b716371f6e011e72c0d637818754e048
brage-andreas <bragerino+github@gmail.com>
2026-02-13 12:24:11 +0100

    refactor: lint

--- frontend/app/routes/layout.tsx
+++ frontend/app/routes/layout.tsx
@@ -429,7 +429,7 @@ const HamburgerIcon = ({
 		<title>{"Menu Icon"}</title>
 		<path
 			d="M4 12L20 12"
-			className="-translate-y-[7px] origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+			className="origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
 		/>
 		<path
 			d="M4 12H20"
