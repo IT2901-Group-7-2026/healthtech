d688f29cdcfc5562e38a2a5337959b68060a99d5
Tobias <94536856+tobiz1004@users.noreply.github.com>
2026-02-24 12:37:20 +0100

    feat: add action card (#104)
    Co-authored-by: Andrefkl <andre.klarpaas@gmail.com>

--- frontend/app/routes/foreman/overview.tsx
+++ frontend/app/routes/foreman/overview.tsx
@@ -19,9 +19,11 @@ import { MapPinIcon, UsersIcon } from "lucide-react";
 import { useQueryState } from "nuqs";
 import { useCallback, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { ActionCard } from "./action-card";
 import { AtRiskPopup } from "./exposure-level-popup";
 import { PieChartCard } from "./pie-chart-card";
 import { StatCard } from "./stat-card";
+import { AtRiskTable } from "./workers-at-risk-table";
 
 // biome-ignore lint/style/noDefaultExport: react router needs default export
 export default function ForemanOverview() {
@@ -141,6 +143,7 @@ export default function ForemanOverview() {
 				</div>
 
 				<div className="flex grow flex-col gap-4">
+					<ActionCard dangerLevel="danger" />
 					<div className="grid gap-6 lg:grid-cols-3">
 						<div className="grid items-stretch gap-4 md:grid-cols-2 lg:col-span-3 lg:grid-cols-3">
 							<StatCard
@@ -199,6 +202,7 @@ export default function ForemanOverview() {
 							/>
 						</div>
 					</div>
+					<AtRiskTable users={subordinates ?? []} />
 
 					{sensor ? (
 						<UserStatusChart
