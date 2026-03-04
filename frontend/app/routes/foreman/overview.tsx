4dc89c70548f5d21f3db429860e74c4851a0c0eb
Andrefkl <andre.klarpaas@gmail.com>
2026-03-05 00:13:25 +0100

    feat: add wip subnavbar in foreman overview

--- frontend/app/routes/foreman/overview.tsx
+++ frontend/app/routes/foreman/overview.tsx
@@ -1,25 +1,34 @@
 /** biome-ignore-all lint/suspicious/noAlert: we allow alerts for testing */
 
-import { useQuery } from "@tanstack/react-query";
-import { MapPinIcon, UsersIcon } from "lucide-react";
-import { useQueryState } from "nuqs";
-import { useCallback, useMemo, useState } from "react";
-import { useTranslation } from "react-i18next";
 import { DailyNotes } from "@/components/daily-notes.js";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import { Card } from "@/components/ui/card";
+import {
+	Combobox,
+	ComboboxContent,
+	ComboboxInput,
+	ComboboxItem,
+	ComboboxList,
+} from "@/components/ui/combobox";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { UserStatusChart } from "@/components/users-status-chart";
 import { useUser } from "@/features/user/user-context";
-import { fetchSubordinatesQueryOptions } from "@/lib/api.js";
+import { fetchSubordinatesQueryOptions, usersQueryOptions } from "@/lib/api.js";
 import type { DangerLevel } from "@/lib/danger-levels";
 import { createLocationName, type UserWithStatusDto } from "@/lib/dto.js";
 import { parseAsSensor, type Sensor, sensors } from "@/lib/sensors";
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "@/ui/select";
+import { useQuery } from "@tanstack/react-query";
+import { addWeeks, format, parseISO, startOfDay } from "date-fns";
+import { ChevronDownIcon, MapPinIcon, UsersIcon } from "lucide-react";
+import { parseAsString, useQueryState } from "nuqs";
+import { useCallback, useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { ActionCard } from "./action-card";
 import { AtRiskPopup } from "./exposure-level-popup";
 import { PieChartCard } from "./pie-chart-card";
@@ -29,6 +38,13 @@ import { AtRiskTable } from "./workers-at-risk-table";
 export default function ForemanOverview() {
 	const { t } = useTranslation();
 	const [sensor, setSensor] = useQueryState("vibration", parseAsSensor);
+	const [date, setDate] = useQueryState("filterDate", parseAsString);
+	const [selectedUser, setSelectedUser] = useQueryState(
+		"user",
+		parseAsString,
+	);
+
+	const minSelectableDate = startOfDay(addWeeks(new Date(), -1));
 
 	const { user } = useUser();
 
@@ -50,6 +66,9 @@ export default function ForemanOverview() {
 	const { data: subordinates } = useQuery(
 		fetchSubordinatesQueryOptions(user.id),
 	);
+
+	const { data: users } = useQuery(usersQueryOptions());
+
 	const addSubordinateDangerLevel = useCallback(
 		(
 			result: Record<Sensor | "total", Record<DangerLevel, number>>,
@@ -93,37 +112,79 @@ export default function ForemanOverview() {
 	);
 	//TODO: Update card links to point to stats page
 
+	const selectedDate = date ? parseISO(date) : undefined;
+
 	return (
 		<div>
-			<div className="mb-4 flex items-center justify-between">
-				<h1 className="p-2 text-3xl">
-					{t(($) => $.foremanDashboard.overview.title)}
-				</h1>
-				<Select
-					onValueChange={(value) => {
-						if (value === "__none") {
-							setSensor(null);
-						} else {
-							setSensor(value as Sensor);
-						}
-					}}
-					value={sensor ?? undefined}
+			<Card className="mb-4 flex flex-row justify-between px-4 py-2">
+				<Tabs
+					className="w-[400px]"
+					value={sensor ?? "all"}
+					onValueChange={(value) => setSensor(value as Sensor | null)}
 				>
-					<SelectTrigger className="w-32 bg-background dark:bg-background">
-						<SelectValue
-							placeholder={t(($) => $.sensorSelectPlaceholder)}
-						/>
-					</SelectTrigger>
-					<SelectContent className="w-32">
-						{sensors?.map((s) => (
-							<SelectItem key={s} value={s}>
-								{t(($) => $[s])}
-							</SelectItem>
-						))}
-						<SelectItem value="__none">{"None"}</SelectItem>
-					</SelectContent>
-				</Select>
-			</div>
+					<TabsList>
+						<TabsTrigger value="all" className="p-4">
+							All
+						</TabsTrigger>
+						<TabsTrigger value="vibration" className="p-4">
+							Vibration
+						</TabsTrigger>
+						<TabsTrigger value="noise" className="p-4">
+							Noise
+						</TabsTrigger>
+						<TabsTrigger value="dust" className="p-4">
+							Dust
+						</TabsTrigger>
+					</TabsList>
+				</Tabs>
+				<div className="flex flex-end flex-row gap-4">
+					<Combobox
+						items={users?.map((u) => u.username) ?? []}
+						value={selectedUser ?? undefined}
+						onValueChange={(value) => setSelectedUser(value)}
+					>
+						<ComboboxInput placeholder="Select a user" showClear />
+						<ComboboxContent>
+							<ComboboxList>
+								{(item) => (
+									<ComboboxItem key={item} value={item}>
+										{item}
+									</ComboboxItem>
+								)}
+							</ComboboxList>
+						</ComboboxContent>
+					</Combobox>
+					<Popover>
+						<PopoverTrigger asChild>
+							<Button
+								variant={"outline"}
+								data-empty={!date}
+								className="w-[212px] justify-between text-left font-normal data-[empty=true]:text-muted-foreground"
+							>
+								{selectedDate ? (
+									format(selectedDate, "PPP")
+								) : (
+									<span>Pick a date</span>
+								)}
+								<ChevronDownIcon data-icon="inline-end" />
+							</Button>
+						</PopoverTrigger>
+						<PopoverContent className="w-auto p-0" align="start">
+							<Calendar
+								mode="single"
+								disabled={{ before: minSelectableDate }}
+								selected={selectedDate}
+								onSelect={(val) =>
+									setDate(
+										val ? format(val, "yyyy-MM-dd") : null,
+									)
+								}
+								defaultMonth={selectedDate}
+							/>
+						</PopoverContent>
+					</Popover>
+				</div>
+			</Card>
 
 			<div className="flex w-full flex-row gap-8">
 				<div className="flex flex-col gap-4 md:w-1/4">
