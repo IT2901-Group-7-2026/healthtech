695efb82b63efaf47b247c75c6b976e891adf9df
ragnhilmd <=>
2026-03-12 13:18:20 +0100

    lint: fix linting

--- frontend/app/routes/foreman/overview.tsx
+++ frontend/app/routes/foreman/overview.tsx
@@ -11,13 +11,12 @@ import {
 	ComboboxItem,
 	ComboboxList,
 } from "@/components/ui/combobox";
+import { DustChart } from "@/components/ui/dust-chart";
 import {
 	Popover,
 	PopoverContent,
 	PopoverTrigger,
 } from "@/components/ui/popover";
-import { endOfMonth, endOfWeek, startOfMonth, startOfWeek } from "date-fns";
-
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { UserStatusChart } from "@/components/users-status-chart";
 import { AttentionCard } from "@/features/attention-card/attention-card.js";
@@ -31,6 +30,7 @@ import {
 	usersQueryOptions,
 } from "@/lib/api.js";
 import { parseAsSensor, type Sensor, sensors } from "@/lib/sensors";
+import { thresholds } from "@/lib/thresholds";
 import { useQuery } from "@tanstack/react-query";
 import { addWeeks, endOfDay, parseISO, startOfDay } from "date-fns";
 import { ChevronDownIcon } from "lucide-react";
@@ -38,11 +38,8 @@ import { parseAsString, useQueryState } from "nuqs";
 import type { ReactNode } from "react";
 import { useTranslation } from "react-i18next";
 import { PieChartCard } from "../../features/attention-card/pie-chart-card";
-import { buildSensorQuery } from "@/lib/queries";
 
-import { DustChart } from "@/components/ui/dust-chart";
-import { thresholds } from "@/lib/thresholds";
-
+// biome-ignore lint/complexity/noExcessiveCognitiveComplexity: help
 export default function ForemanOverview() {
 	const { t } = useTranslation();
 	const formatDate = useFormatDate();
@@ -54,7 +51,10 @@ export default function ForemanOverview() {
 	);
 
 	// TODO: Use this to show data for only that user
-	const [selectedUser, setSelectedUser] = useQueryState("user", parseAsString);
+	const [selectedUser, setSelectedUser] = useQueryState(
+		"user",
+		parseAsString,
+	);
 
 	const selectedDate = date ? parseISO(date) : undefined;
 
@@ -116,7 +116,9 @@ export default function ForemanOverview() {
 	);
 
 	const { data: thresholdSummary, isLoading: isThresholdSummaryLoading } =
-		useQuery(fetchThresholdSummaryQueryOptions(user.id, startDate, endDate));
+		useQuery(
+			fetchThresholdSummaryQueryOptions(user.id, startDate, endDate),
+		);
 
 	const subordinateCount = subordinates?.length ?? 0;
 	const isUserComboboxDisabled = !users || users.length === 0;
@@ -157,7 +159,9 @@ export default function ForemanOverview() {
 					>
 						<ComboboxInput
 							placeholder={t(
-								($) => $.foremanDashboard.overview.selectUserPlaceholder,
+								($) =>
+									$.foremanDashboard.overview
+										.selectUserPlaceholder,
 							)}
 							showClear
 							disabled={isUserComboboxDisabled}
@@ -186,7 +190,9 @@ export default function ForemanOverview() {
 								) : (
 									<span>
 										{t(
-											($) => $.foremanDashboard.overview.selectDatePlaceholder,
+											($) =>
+												$.foremanDashboard.overview
+													.selectDatePlaceholder,
 										)}
 									</span>
 								)}
@@ -202,7 +208,11 @@ export default function ForemanOverview() {
 								}}
 								selected={selectedDate}
 								onSelect={(val) =>
-									setDate(val ? formatDate(val, "yyyy-MM-dd") : null)
+									setDate(
+										val
+											? formatDate(val, "yyyy-MM-dd")
+											: null,
+									)
 								}
 								defaultMonth={selectedDate}
 							/>
@@ -244,28 +254,38 @@ export default function ForemanOverview() {
 											data={{
 												safe: {
 													name: "Safe",
-													value: thresholdSummary[s].safe,
+													value: thresholdSummary[s]
+														.safe,
 													label: t(
 														($) =>
-															$.foremanDashboard.overview.statCards.safe.label,
+															$.foremanDashboard
+																.overview
+																.statCards.safe
+																.label,
 													),
 												},
 												warning: {
 													name: "Warning",
-													value: thresholdSummary[s].warning,
+													value: thresholdSummary[s]
+														.warning,
 													label: t(
 														($) =>
-															$.foremanDashboard.overview.statCards.warning
-																.label,
+															$.foremanDashboard
+																.overview
+																.statCards
+																.warning.label,
 													),
 												},
 												danger: {
 													name: "Danger",
-													value: thresholdSummary[s].danger,
+													value: thresholdSummary[s]
+														.danger,
 													label: t(
 														($) =>
-															$.foremanDashboard.overview.statCards.danger
-																.label,
+															$.foremanDashboard
+																.overview
+																.statCards
+																.danger.label,
 													),
 												},
 											}}
@@ -277,37 +297,33 @@ export default function ForemanOverview() {
 								)}
 						</div>
 					)}
-					{
-						selectedUser && (
+					{selectedUser && (
+						<div className="flex flex-row items-center gap-8">
+							{dustTwa1Data && dustTwa1Data.length > 0 && (
+								<DustChart
+									label="PM1 TWA"
+									value={dustTwa1Data[0].value}
+									thresholdValue={thresholds.dust.danger}
+								/>
+							)}
 
-							<div className="flex flex-row items-center gap-8">
-						{dustTwa1Data && dustTwa1Data.length > 0 && (
-							<DustChart
-							label="PM1 TWA"
-							value={dustTwa1Data[0].value}
-							thresholdValue={thresholds["dust"].danger}
-							/>
-						)}
+							{dustTwa25Data && dustTwa25Data.length > 0 && (
+								<DustChart
+									label="PM2.5 TWA"
+									value={dustTwa25Data[0].value}
+									thresholdValue={thresholds.dust.danger}
+								/>
+							)}
 
-						{dustTwa25Data && dustTwa25Data.length > 0 && (
-							<DustChart
-							label="PM2.5 TWA"
-							
-							value={dustTwa25Data[0].value}
-							thresholdValue={thresholds["dust"].danger}
-							/>
-						)}
-
-						{dustTwa10Data && dustTwa10Data.length > 0 && (
-							<DustChart
-							label="PM10 TWA"
-							value={dustTwa10Data[0].value}
-							thresholdValue={thresholds["dust"].danger}
-							/>
-						)}
-					</div>
-						)
-					}
+							{dustTwa10Data && dustTwa10Data.length > 0 && (
+								<DustChart
+									label="PM10 TWA"
+									value={dustTwa10Data[0].value}
+									thresholdValue={thresholds.dust.danger}
+								/>
+							)}
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
