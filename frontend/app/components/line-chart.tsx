dd8e55d54f5f75d54e0d4b1d18f1982cc08c495c
WilliamTjonndal <54414193+WilliamTjonndal@users.noreply.github.com>
2026-03-13 11:58:49 +0100

    feat: revert to original line graph colors (#192)
    Co-authored-by: William Henry Tjønndal <williaht@stud.ntnu.no>

--- frontend/app/components/line-chart.tsx
+++ frontend/app/components/line-chart.tsx
@@ -6,7 +6,6 @@ import {
 	ChartTooltip,
 	ChartTooltipContent,
 } from "@/components/ui/chart";
-import { useTheme } from "@/features/dark-mode/use-theme";
 import { useDate } from "@/features/date-picker/use-date";
 import { type DangerLevel, DangerLevels } from "@/lib/danger-levels";
 import type { SensorDataResponseDto, UserSensorStatusDto } from "@/lib/dto";
@@ -19,7 +18,6 @@ import {
 	CartesianGrid,
 	Line,
 	LineChart,
-	ReferenceArea,
 	ReferenceLine,
 	XAxis,
 	YAxis,
@@ -96,12 +94,6 @@ export function ChartLineDefault({
 		new Date(time).getUTCHours().toString().padStart(2, "0");
 
 	const maxDataDangerLevel = getDangerLevel(maxData, usePeakData);
-
-	const { theme } = useTheme();
-	const color =
-		theme === "dark"
-			? "oklch(90% 0.019 276.296)"
-			: "oklch(35% 0.015 286.067)";
 
 	return (
 		<Card className="w-full">
@@ -194,54 +186,31 @@ export function ChartLineDefault({
 												offset={getOffset(
 													dangerThreshold,
 												)}
-												stopColor={color}
+												stopColor={"var(--danger)"}
 											/>
 											<stop
 												offset={getOffset(
 													dangerThreshold,
 												)}
-												stopColor={color}
+												stopColor={"var(--warning)"}
 											/>
 											<stop
 												offset={getOffset(warning)}
-												stopColor={color}
+												stopColor={"var(--warning)"}
 											/>
 											<stop
 												offset={getOffset(warning)}
-												stopColor={color}
+												stopColor={"var(--safe)"}
 											/>
 											<stop
 												offset="100%"
-												stopColor={color}
+												stopColor={"var(--safe)"}
 											/>
 										</>
 									)
 								)}
 							</linearGradient>
 						</defs>
-						{/* Safe zone */}
-						<ReferenceArea
-							y1={minY}
-							y2={warning}
-							fill="var(--safe)"
-							fillOpacity={0.08}
-						/>
-
-						{/* Warning zone */}
-						<ReferenceArea
-							y1={warning}
-							y2={dangerThreshold}
-							fill="var(--warning)"
-							fillOpacity={0.08}
-						/>
-
-						{/* Danger zone */}
-						<ReferenceArea
-							y1={dangerThreshold}
-							y2={maxY}
-							fill="var(--danger)"
-							fillOpacity={0.08}
-						/>
 						<Line
 							dataKey="value"
 							type={lineType as CurveType}
