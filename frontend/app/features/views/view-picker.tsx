d3a236639683f4a31dba6ca3cfc21550ec86cc7e
Andrefkl <andre.klarpaas@gmail.com>
2026-03-21 16:35:03 +0100

    feat: show selected date in view picker

--- frontend/app/features/views/view-picker.tsx
+++ frontend/app/features/views/view-picker.tsx
@@ -1,3 +1,5 @@
+import { useFormatDate } from "@/hooks/use-format-date";
+import { capitalize } from "@/lib/utils";
 import {
 	Select,
 	SelectContent,
@@ -5,19 +7,26 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/ui/select";
+import { endOfWeek, startOfWeek } from "date-fns";
 import { t } from "i18next";
+import { useDate } from "../date-picker/use-date";
 import { useView } from "./use-view";
 import type { View } from "./utils";
 import { views } from "./utils";
 
 export function ViewPicker() {
+	const formatDate = useFormatDate();
 	const { view, setView } = useView();
+	const { date } = useDate();
+
 	return (
 		<Select value={view} onValueChange={(value: View) => setView(value)}>
-			<SelectTrigger className="w-32">
-				<SelectValue placeholder="View" />
+			<SelectTrigger className="w-50">
+				<SelectValue>
+					{formatSelectedView(view, date, formatDate)}
+				</SelectValue>
 			</SelectTrigger>
-			<SelectContent className="w-32">
+			<SelectContent className="w-50">
 				{views.map((v: View) => (
 					<SelectItem key={v} value={v}>
 						{t(($) => $[v])}
@@ -26,4 +35,26 @@ export function ViewPicker() {
 			</SelectContent>
 		</Select>
 	);
+}
+
+function formatSelectedView(
+	view: View,
+	date: Date,
+	formatDate: ReturnType<typeof useFormatDate>,
+) {
+	if (view === "day") {
+		return formatDate(date, "PPP");
+	}
+
+	if (view === "week") {
+		const start = startOfWeek(date, { weekStartsOn: 1 });
+		const end = endOfWeek(date, { weekStartsOn: 1 });
+
+		const startStr = formatDate(start, "d MMM");
+		const endStr = formatDate(end, "d MMM yyyy");
+
+		return `${startStr} - ${endStr}`;
+	}
+
+	return capitalize(formatDate(date, "LLLL yyyy"));
 }
