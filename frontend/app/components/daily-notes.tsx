13c00a9e542f2b1e4d5de6161c8463824e830f10
Andrefkl <andre.klarpaas@gmail.com>
2026-04-05 21:50:43 +0200

    refactor: daily notes

--- frontend/app/components/daily-notes.tsx
+++ frontend/app/components/daily-notes.tsx
@@ -1,5 +1,3 @@
-/** biome-ignore-all lint/complexity/noExcessiveCognitiveComplexity: <I prefer this over sending functions as props> */
-
 import { useDate } from "@/features/date-picker/use-date";
 import { useUser } from "@/features/user/user-context";
 import { useView } from "@/features/views/use-view";
@@ -9,17 +7,13 @@ import { createNote, notesQueryOptions, updateNote } from "@/lib/api";
 import type { Note } from "@/lib/dto";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { isSameDay } from "date-fns";
-import { useEffect, useState } from "react";
+import { type JSX, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
 import { Textarea } from "./ui/textarea";
 
-export const DailyNotes = ({
-	popUpOverride = false,
-}: {
-	popUpOverride?: boolean;
-}) => {
+export const DailyNotes = ({ popUpOverride = false }: { popUpOverride?: boolean }) => {
 	const { t, i18n } = useTranslation();
 	const locale = i18n.language;
 	const { view } = useView();
@@ -48,16 +42,11 @@ export const DailyNotes = ({
 	});
 
 	const [todayNote, setTodayNote] = useState<Note | null>(
-		data
-			? (data.find((note) => isSameDay(note.time, date, { in: TIMEZONE })) ??
-					null)
-			: null,
+		data ? (data.find((note) => isSameDay(note.time, date, { in: TIMEZONE })) ?? null) : null,
 	);
 
 	const [showTextArea, setShowTextArea] = useState<boolean>(
-		data
-			? !data.some((note) => isSameDay(note.time, date, { in: TIMEZONE }))
-			: true,
+		data ? !data.some((note) => isSameDay(note.time, date, { in: TIMEZONE })) : true,
 	);
 
 	const handleEdit = () => {
@@ -77,9 +66,7 @@ export const DailyNotes = ({
 
 	useEffect(() => {
 		if (data) {
-			const foundNote =
-				data.find((note) => isSameDay(note.time, date, { in: TIMEZONE })) ??
-				null;
+			const foundNote = data.find((note) => isSameDay(note.time, date, { in: TIMEZONE })) ?? null;
 			setTodayNote(foundNote);
 			setShowTextArea(!foundNote);
 		}
@@ -103,44 +90,81 @@ export const DailyNotes = ({
 		);
 	}
 
-	if (view === "day" || popUpOverride) {
-		return (
-			<Card muted={true} className="max-h-96 w-full overflow-y-auto">
-				<CardHeader>
-					<h2 className="text-muted-foreground text-xs uppercase tracking-wider">
-						{t(($) => $.daily_notes.dayTitle, {
-							day: formatDate(date, locale === "en" ? "MMMM do" : "do MMMM"),
-						})}
-					</h2>
-				</CardHeader>
-				<CardContent>
-					{showTextArea ? (
-						<Textarea
-							placeholder={t(($) => $.daily_notes.writeHere)}
-							value={todayNote?.note ?? ""}
-							className="min-h-15"
-							onChange={(e) =>
-								setTodayNote({
-									time: date,
-									note: e.target.value,
-								})
-							}
-						/>
-					) : (
-						<p>
-							{
-								data?.find((note) =>
-									isSameDay(note.time, date, {
-										in: TIMEZONE,
-									}),
-								)?.note
-							}
-						</p>
-					)}
-				</CardContent>
+	const isForDayView = view === "day" || popUpOverride;
+
+	let titleFormattedDate = "";
+	switch (view) {
+		case "day":
+			titleFormattedDate = formatDate(date, locale === "en" ? "MMMM do" : "do MMMM");
+			break;
+		case "week":
+			titleFormattedDate = `${t(($) => $.week)} ${formatDate(date, "w, yyyy")}`;
+			break;
+		case "month":
+			titleFormattedDate = formatDate(date, "MMMM yyyy");
+			break;
+	}
+
+	const title = t(($) => $.daily_notes.viewTitle, {
+		view: titleFormattedDate,
+	});
+
+	let Content: JSX.Element;
+
+	if (isForDayView) {
+		Content =
+			isForDayView && showTextArea ? (
+				<Textarea
+					placeholder={t(($) => $.daily_notes.writeHere)}
+					value={todayNote?.note ?? ""}
+					className="min-h-15"
+					onChange={(e) =>
+						setTodayNote({
+							time: date,
+							note: e.target.value,
+						})
+					}
+				/>
+			) : (
+				<p>
+					{
+						data?.find((note) =>
+							isSameDay(note.time, date, {
+								in: TIMEZONE,
+							}),
+						)?.note
+					}
+				</p>
+			);
+	} else {
+		Content = (
+			<ul>
+				{data?.map((note) => (
+					<li key={note.time.getTime()}>
+						<strong>
+							{note.time.toLocaleDateString(locale, {
+								day: "numeric",
+								month: "long",
+							})}
+							{": "}
+						</strong>
+						{note.note}
+					</li>
+				))}
+			</ul>
+		);
+	}
+
+	return (
+		<Card muted={true} className="max-h-96 w-full overflow-y-auto">
+			<CardHeader>
+				<h2 className="text-muted-foreground text-xs uppercase tracking-wider">{title}</h2>
+			</CardHeader>
+			<CardContent>{Content}</CardContent>
+			{isForDayView && (
 				<CardFooter className="justify-end gap-2">
 					{todayNote !== null && !showTextArea && (
-						<Button size="sm" variant={"secondary"} onClick={handleEdit}>
+						<Button size="sm" variant="secondary" onClick={handleEdit}>
 							{t(($) => $.daily_notes.edit)}
 						</Button>
 					)}
@@ -154,70 +178,7 @@ export const DailyNotes = ({
 						</Button>
 					)}
 				</CardFooter>
-			</Card>
-		);
-	}
-
-	if (view === "week") {
-		return (
-			<Card muted={true} className="max-h-96 w-full overflow-y-auto">
-				<CardHeader>
-					<h2 className="text-xl">
-						{t(($) => $.daily_notes.notesFromThis)}
-						<strong>{t(($) => $.daily_notes.week)}</strong>
-						{":"}
-					</h2>
-				</CardHeader>
-				<CardContent>
-					<ul>
-						{data
-							? data.map((note) => (
-									<li key={note.time.toDateString()}>
-										<strong>
-											{note.time.toLocaleDateString(locale, {
-												day: "numeric",
-												month: "long",
-											})}
-											{": "}
-										</strong>
-										{note.note}
-									</li>
-								))
-							: null}
-					</ul>
-				</CardContent>
-			</Card>
-		);
-	}
-
-	//month-view
-	return (
-		<Card muted={true} className="max-h-96 w-full overflow-y-auto">
-			<CardHeader>
-				<h2 className="text-xl">
-					{t(($) => $.daily_notes.notesFromThis)}
-					<strong>{t(($) => $.daily_notes.month)}</strong>
-					{":"}
-				</h2>
-			</CardHeader>
-			<CardContent>
-				<ul>
-					{data
-						? data.map((note) => (
-								<li key={note.time.getTime()}>
-									<strong>
-										{note.time.toLocaleDateString(locale, {
-											day: "numeric",
-											month: "long",
-										})}
-										{": "}
-									</strong>
-									{note.note}
-								</li>
-							))
-						: null}
-				</ul>
-			</CardContent>
+			)}
 		</Card>
 	);
 };
