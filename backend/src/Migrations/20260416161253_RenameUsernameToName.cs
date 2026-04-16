08b628cf79b9ceade6a3c83e98d1582c2333a019
Andrefkl <andre.klarpaas@gmail.com>
2026-04-16 18:22:01 +0200

    refactor: lint

--- backend/src/Migrations/20260416161253_RenameUsernameToName.cs
+++ backend/src/Migrations/20260416161253_RenameUsernameToName.cs
@@ -4,35 +4,31 @@
 
 namespace backend.Migrations
 {
-    /// <inheritdoc />
-    public partial class RenameUsernameToName : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "Username",
-                table: "User",
-                newName: "Name");
+	/// <inheritdoc />
+	public partial class RenameUsernameToName : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(name: "Username", table: "User", newName: "Name");
 
-            migrationBuilder.RenameIndex(
-                name: "IX_User_Username",
-                table: "User",
-                newName: "IX_User_Name");
-        }
+			migrationBuilder.RenameIndex(
+				name: "IX_User_Username",
+				table: "User",
+				newName: "IX_User_Name"
+			);
+		}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "Name",
-                table: "User",
-                newName: "Username");
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(name: "Name", table: "User", newName: "Username");
 
-            migrationBuilder.RenameIndex(
-                name: "IX_User_Name",
-                table: "User",
-                newName: "IX_User_Username");
-        }
-    }
+			migrationBuilder.RenameIndex(
+				name: "IX_User_Name",
+				table: "User",
+				newName: "IX_User_Username"
+			);
+		}
+	}
 }
