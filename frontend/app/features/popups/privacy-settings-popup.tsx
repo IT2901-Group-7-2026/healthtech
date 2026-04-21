5b64f20680e7bbb9c04cb8282916bee68cd3e446
William Henry Tjønndal <williaht@stud.ntnu.no>
2026-04-21 11:56:38 +0200

    feat: update dropdown menu in layout.tsx with privacy settings section

---
+++ frontend/app/features/popups/privacy-settings-popup.tsx
@@ -0,0 +1,239 @@
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Card } from "@/components/ui/card";
+import { Form, FormField, FormItem } from "@/components/ui/form";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { useFormatDate } from "@/hooks/use-format-date";
+import { now, toTZDate } from "@/lib/date";
+import type { User } from "@/lib/dto.js";
+import { userRoleToString } from "@/lib/utils.js";
+import { TZDate } from "@date-fns/tz";
+import { isBefore } from "date-fns";
+import { ChevronDownIcon } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { BasePopup } from "./base-popup";
+
+interface PrivacySettingsPopupProps {
+	user: User;
+	avatarSrc: string;
+	open: boolean;
+	onClose: () => void;
+	users?: Array<User>;
+	setUser?: (user: User) => void;
+	children?: React.ReactNode;
+}
+
+export function PrivacySettingsPopup({ user, open, onClose, avatarSrc, children }: PrivacySettingsPopupProps) {
+	const { t } = useTranslation();
+	const title = t(($) => $.profile.title);
+
+	const tempListOfRegulations = ["Safety boots", "Helmet", "Protective mask"];
+	type FormValues = {
+		fromDate?: TZDate;
+		toDate?: TZDate;
+	};
+
+	const form = useForm<FormValues>({
+		defaultValues: {
+			fromDate: undefined,
+			toDate: undefined,
+		},
+		mode: "onChange",
+	});
+
+	const fromDate = form.watch("fromDate");
+	const toDate = form.watch("toDate");
+	const format = useFormatDate();
+
+	const onSubmit = (data: FormValues) => {
+		if (data.fromDate && data.toDate && isBefore(data.toDate, data.fromDate)) {
+			form.setError("toDate", {
+				message: t(($) => $.popup.invalidDate),
+			});
+
+			setTimeout(() => {
+				form.clearErrors("toDate");
+			}, 5000);
+			return;
+		}
+
+		setDeleteText(true);
+
+		setTimeout(() => {
+			setDeleteText(false);
+		}, 5000);
+	};
+
+	const minSelectableDate = new TZDate(2024, 0, 1, "Europe/Oslo");
+	const maxSelectableDate = now();
+
+	const [fromCalendarOpen, setFromCalendarOpen] = useState(false);
+	const [toCalendarOpen, setToCalendarOpen] = useState(false);
+	const [deleteText, setDeleteText] = useState(false);
+
+	return (
+		<BasePopup title={title} open={open} relevantDate={null} onClose={onClose}>
+			{children}
+
+			<div className="flex flex-col gap-3 text-sm md:px-0 md:pb-1">
+				<div className="flex flex-row items-center gap-4">
+					<div>
+						{avatarSrc ? (
+							<img
+								src={avatarSrc}
+								height={100}
+								width={100}
+								alt={user.name}
+								className="h-full w-full rounded-full object-cover"
+							/>
+						) : (
+							<div className="h-20 w-20 rounded-full bg-blue-900"></div>
+						)}
+					</div>
+					<div className="flex w-full flex-col">
+						<div className="flex flex-row gap-3">
+							<p className="label text-muted-foreground">{t(($) => $.profile.name)}</p>
+							<h2>{user.name}</h2>
+						</div>
+						<div className="flex flex-row gap-3">
+							<p className="label text-muted-foreground">{t(($) => $.profile.location)}</p>
+							<h3>{user.location.site}</h3>
+						</div>
+						<div className="flex flex-row gap-3">
+							<p className="label text-muted-foreground">{t(($) => $.profile.jobTitle)}</p>
+							<h3>{userRoleToString(user.role, t)}</h3>
+						</div>
+					</div>
+				</div>
+				<div className="flex flex-col gap-2">
+					<h4 className="text-muted-foreground">{t(($) => $.profile.currentSecurityRegulations)}</h4>
+					<div className="w-full rounded-lg bg-card-highlight p-2">
+						<ul className="ml-5 list-disc">
+							{tempListOfRegulations.map((reg) => (
+								<li key={reg}>{reg}</li>
+							))}
+						</ul>
+					</div>
+				</div>
+				<div className="flex flex-col gap-2">
+					<p className="label text-muted-foreground">{t(($) => $.profile.jobDescription)}</p>
+					<div className="w-full rounded-lg bg-card-highlight p-2">
+						<p>{user.jobDescription}</p>
+					</div>
+				</div>
+				<div className="flex flex-col gap-2">
+					<p className="label text-muted-foreground">{t(($) => $.profile.deletePersonalInformation)}</p>
+					<Card className="p-3">
+						<Form {...form}>
+							<form onSubmit={form.handleSubmit(onSubmit)} className="max-w-sm space-y-2">
+								{/* From */}
+								<div className="flex flex-col gap-2">
+									<span>{t(($) => $.popup.from)}</span>
+									<FormField
+										control={form.control}
+										name="fromDate"
+										render={({ field }) => (
+											<FormItem>
+												<Popover open={fromCalendarOpen} onOpenChange={setFromCalendarOpen}>
+													<PopoverTrigger asChild={true}>
+														<Button
+															variant="outline"
+															className="h-8 w-32 justify-between text-sm"
+														>
+															{fromDate
+																? format(fromDate, "dd.MM.yy")
+																: t(($) => $.popup.startDate)}
+															<ChevronDownIcon />
+														</Button>
+													</PopoverTrigger>
+													<PopoverContent className="w-auto p-0">
+														<Calendar
+															mode="single"
+															selected={field.value}
+															onSelect={(value) =>
+																field.onChange(value ? toTZDate(value) : undefined)
+															}
+															disabled={{
+																before: minSelectableDate,
+																after: maxSelectableDate,
+															}}
+														/>
+													</PopoverContent>
+												</Popover>
+											</FormItem>
+										)}
+									/>
+								</div>
+
+								{/* To */}
+								<div className="flex flex-col gap-2">
+									<span>{t(($) => $.popup.to)}</span>
+									<FormField
+										control={form.control}
+										name="toDate"
+										render={({ field }) => (
+											<FormItem>
+												<Popover open={toCalendarOpen} onOpenChange={setToCalendarOpen}>
+													<PopoverTrigger asChild={true}>
+														<Button
+															variant="outline"
+															className="h-8 w-32 justify-between text-sm"
+														>
+															{toDate
+																? format(toDate, "dd.MM.yy")
+																: t(($) => $.popup.endDate)}
+															<ChevronDownIcon />
+														</Button>
+													</PopoverTrigger>
+													<PopoverContent className="w-auto p-0">
+														<Calendar
+															mode="single"
+															selected={field.value}
+															onSelect={(value) =>
+																field.onChange(value ? toTZDate(value) : undefined)
+															}
+															disabled={{
+																before: minSelectableDate,
+																after: maxSelectableDate,
+															}}
+														/>
+													</PopoverContent>
+												</Popover>
+											</FormItem>
+										)}
+									/>
+								</div>
+
+								{/* Button + floating message */}
+								<div className="relative mt-10">
+									<Button type="submit" disabled={!(fromDate && toDate)} className="h-8 text-sm">
+										{t(($) => $.popup.deleteData)}
+									</Button>
+
+									<div className="absolute bottom-full left-0 mb-2">
+										{form.formState.errors.toDate && (
+											<div className="text-red-500 text-sm">
+												{form.formState.errors.toDate.message}
+											</div>
+										)}
+
+										{deleteText && fromDate && toDate && (
+											<div className="text-green-700 text-sm">
+												{t(($) => $.popup.dataDeleted, {
+													from: format(fromDate, "d MMMM yyyy"),
+													to: format(toDate, "d MMMM yyyy"),
+												})}
+											</div>
+										)}
+									</div>
+								</div>
+							</form>
+						</Form>
+					</Card>
+				</div>
+			</div>
+		</BasePopup>
+	);
+}
--- frontend/app/routes/layout.tsx
+++ frontend/app/routes/layout.tsx
@@ -16,6 +16,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useTheme } from "@/features/dark-mode/use-theme";
 import { useDate } from "@/features/date-picker/use-date";
 import { BellPopup } from "@/features/popups/bell-popup";
+import { PrivacySettingsPopup } from "@/features/popups/privacy-settings-popup";
 import { ProfilePopup } from "@/features/popups/profile-popup";
 import { usePopup } from "@/features/popups/use-popup";
 import { useUser } from "@/features/user/user-context";
@@ -28,7 +29,17 @@ import { type User, UserRoleSchema } from "@/lib/dto.js";
 import { cn, shorthandName, userRoleToString } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import "leaflet/dist/leaflet.css";
-import { Bell, Languages, type LucideIcon, Monitor, Moon, Palette, Sun, User as UserIcon } from "lucide-react";
+import {
+	Bell,
+	HatGlassesIcon,
+	Languages,
+	type LucideIcon,
+	Monitor,
+	Moon,
+	Palette,
+	Sun,
+	User as UserIcon,
+} from "lucide-react";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { href, Link, NavLink, Outlet, type To, useLocation, useNavigate } from "react-router";
@@ -104,11 +115,9 @@ function getLinks(t: TranslateFn, role: User["role"] | null): Array<{ to: To; la
 
 export default function Layout() {
 	const { t, i18n } = useTranslation();
-	const {
-		visible: notificationPopupVisible,
-		openPopup: openNotificationPopup,
-		closePopup: closeNotificationPopup,
-	} = usePopup();
+	const { visible, openPopup, closePopup } = usePopup();
+
+	const openNotificationPopup = () => openPopup("notifications");
 
 	const { user, setUser, isLoading: isUserLoading } = useUser();
 	const { data: users } = useQuery(usersQueryOptions());
@@ -196,8 +205,8 @@ export default function Layout() {
 					</header>
 
 					<BellPopup
-						open={notificationPopupVisible}
-						onClose={closeNotificationPopup}
+						open={visible === "notifications"}
+						onClose={closePopup}
 						title={t(($) => $.common.notifications)}
 					/>
 
@@ -225,6 +234,9 @@ function UserDropdown({
 	const { theme, setTheme } = useTheme();
 	const { visible, openPopup, closePopup } = usePopup();
 
+	const openProfile = () => openPopup("profile");
+	const openPrivacySettings = () => openPopup("privacySettings");
+
 	if (!user) return null;
 
 	const currentLanguage = i18n.language || "en";
@@ -245,9 +257,18 @@ function UserDropdown({
 				</DropdownMenuTrigger>
 
 				<DropdownMenuContent align="end" className="" sideOffset={10}>
-					<DropdownMenuItem onSelect={() => openPopup()}>
+					{/* My account */}
+					<DropdownMenuItem onSelect={() => openProfile}>
 						<UserIcon className="size-4" />
 						<span>{t(($) => $.profile.title)}</span>
+					</DropdownMenuItem>
+
+					<DropdownMenuSeparator />
+
+					{/* Privacy settings */}
+					<DropdownMenuItem onSelect={() => openPrivacySettings}>
+						<HatGlassesIcon className="size-4" />
+						<span>{t(($) => $.profile.privacySettings)}</span>
 					</DropdownMenuItem>
 
 					<DropdownMenuSeparator />
@@ -376,7 +397,16 @@ function UserDropdown({
 			<ProfilePopup
 				user={user}
 				avatarSrc="/userimage.png"
-				open={visible}
+				open={visible === "profile"}
+				onClose={closePopup}
+				users={users}
+				setUser={setUser}
+			/>
+
+			<PrivacySettingsPopup
+				user={user}
+				avatarSrc="/userimage.png"
+				open={visible === "privacySettings"}
 				onClose={closePopup}
 				users={users}
 				setUser={setUser}
